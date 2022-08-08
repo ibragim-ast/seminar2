9406b2a6b8690108f00efe213b735836d584bd96
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-08 20:58:38 +0300

    add task003

---
+++ Task001/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine(number);
+int digit1 = number / 10;
+int digit2 = number % 10;
+if (digit1 > digit2)
+    Console.WriteLine(digit1);
+else
+    Console.WriteLine(digit2);
---
+++ Task002/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int digit1 = (number / 100) * 10 + number % 10;
+
+Console.Write(digit1);
+
+
