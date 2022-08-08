adcdefe259ae5e8e3eca862dd39e490c2315845d
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-09 01:01:59 +0300

    add homework013

---
+++ Homework013/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите цифру больше 99");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+    Console.WriteLine("Нужно больше циферок, милорд"); 
+else 
+    {
+    while (number > 99)
+        number = number / 10;
+
+    int result = number % 10;
+    Console.WriteLine(result);
+    }
